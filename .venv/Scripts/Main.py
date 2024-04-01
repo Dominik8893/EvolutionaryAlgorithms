605404c7fb81f46332c7cae6be7bc5adb6a5aed7
Dominik8893 <cyberdomi8894@gmail.com>
2024-04-01 15:28:09 +0200

    Lab 1 now working as intended

--- .venv/Scripts/Main.py
+++ .venv/Scripts/Main.py
@@ -1,20 +1,5 @@
 import numpy as np
 
-# Function to calculate distance between 2 points
-def DistanceBetweenCities(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-
-# Function to calculate distance for the entire Path
-def TourDistance(Path, CitiesX, CitiesY):
-    TotalDistance = 0
-    for i in range(len(Path)):
-        if i == len(Path) - 1:
-            TotalDistance += DistanceBetweenCities(CitiesX[Path[i]], CitiesY[Path[i]], CitiesX[Path[0]], CitiesY[Path[0]])
-        else:
-            TotalDistance += DistanceBetweenCities(CitiesX[Path[i]], CitiesY[Path[i]], CitiesX[Path[i+1]], CitiesY[Path[i+1]])
-    print(TotalDistance)
-    return TotalDistance
-
 # Cities coordinates
 c1_x = np.array([0, 3, 6, 7, 15, 12, 14, 9, 7, 0])
 c1_y = np.array([1, 4, 5, 3, 0, 4, 10, 6, 9, 10])
@@ -33,15 +18,116 @@ c4_y = np.array([1, 4, 2, 4.5, 9, 1.5, 11, 8, 10, 7]);
 
 
 # Population size
-size = 250
+Size = 250
+ParentSelectionAmount = 0.2
+IterationAmount = 50
+
+
+
+
+# Function to calculate distance between 2 points
+def DistanceBetweenCities(x1, y1, x2, y2):
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+# Function to calculate distance for the entire Path
+def TourDistance(Path):
+    CitiesX = c4_x
+    CitiesY = c4_y
+    TotalDistance = 0
+    for i in range(len(Path)):
+        if i == len(Path) - 1:
+            TotalDistance += DistanceBetweenCities(CitiesX[Path[i]], CitiesY[Path[i]], CitiesX[Path[0]], CitiesY[Path[0]])
+        else:
+            TotalDistance += DistanceBetweenCities(CitiesX[Path[i]], CitiesY[Path[i]], CitiesX[Path[i+1]], CitiesY[Path[i+1]])
+    return TotalDistance
+
+
+def CycleCrossover(Parent1, Parent2):
+    Offspring1 = [None] * len(Parent1)
+    Offspring2 = [None] * len(Parent2)
+
+    # Choose a random starting point
+    start_index = np.random.randint(0, len(Parent1))
+    index = start_index
+
+    # Perform cycle crossover
+    while True:
+        # Copy the selected elements from parent 1 to offspring 1
+        Offspring1[index] = Parent1[index]
+
+        # Find the element from parent 2 corresponding to the current index
+        element = Parent2[index]
+        index = np.where(Parent1 == element)[0][0]
+
+        # Check if the cycle is complete
+        if index == start_index:
+            break
+
+    # Fill in the remaining elements for offspring 2
+    for i in range(len(Offspring2)):
+        if Offspring1[i] is None:
+            Offspring1[i] = Parent2[i]
+            Offspring2[i] = Parent1[i]
+        else:
+            Offspring2[i] = Parent1[i]
+
+    return Offspring1, Offspring2
+
+def Mutate(ElementToBeMutated):
+    #select Elements to swap
+    FirstSwap = np.random.randint(0,9)
+    SecondSwap = np.random.randint(0, 9)
+
+    #perform Swap
+    Temp = np.copy(ElementToBeMutated)
+    ElementToBeMutated[FirstSwap] = Temp[SecondSwap]
+    ElementToBeMutated[SecondSwap] = Temp[FirstSwap]
+
+    return ElementToBeMutated
+
+
+
+
 
 # Initialize population
-Population = np.zeros((size, 10), dtype=int)
+Population = np.zeros((Size, 10),dtype = int )
+Distances = np.zeros(Size)
 
 # Generate random population
-for i in range(size):
+for i in range(Size):
     random_arr = np.random.permutation(10)
     Population[i, :] = random_arr
+    Distances[i] = TourDistance(Population[i])
 
-# Calculate distances for the first individual in the population
-distances = TourDistance(Population[0], c3_x, c3_y)
+for j in range(IterationAmount):
+    SelectedParents = np.zeros((round(Size*ParentSelectionAmount), 10),dtype = int )
+    for i in range(round(Size*ParentSelectionAmount)):
+        SelectedParents[i] = Population[i]
+
+    Children = np.zeros((round(Size*ParentSelectionAmount), 10),dtype = int )
+    for i in range(round((Size*ParentSelectionAmount)/2)):
+
+        Parent1 = SelectedParents[np.random.choice(SelectedParents.shape[0])]
+        Parent2 = SelectedParents[np.random.choice(SelectedParents.shape[0])]
+
+        Children[i], Children[i + round((Size*ParentSelectionAmount)/2)] = CycleCrossover(Parent1, Parent2)
+
+    #mutate children
+    for i in range(round(Size*ParentSelectionAmount)):
+        Children[i] = Mutate(Children[i])
+
+
+    Population = np.concatenate((Population, Children), axis=0)
+
+    #sorting the population by best distance
+    Population = sorted(Population, key=TourDistance)
+
+    #cutting off excess population
+    Population = Population[:Size]
+
+    print("Best Paren in " + str(j) + "  " + str(TourDistance(Population[0])))
+
+
+
+
+
